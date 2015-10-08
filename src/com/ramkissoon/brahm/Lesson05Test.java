d15add8e54799c3bded4e3b303e486ae305b0d09
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-10-08 15:11:27 -0400

    added student, course classes

--- src/com/ramkissoon/brahm/Lesson05Test.java
+++ src/com/ramkissoon/brahm/Lesson05Test.java
@@ -5,16 +5,22 @@ public class Lesson05Test {
 	
 	// MAIN STATIC METHOD ++++++++++++++++++++++
 	public static void main(String[] args) {
-		Resident resident = new Resident("Brahm", 20, 72.0f, 200.0f, "555 555 555");
+		Student student = new Student("Brahm", 20, 72.0f, 200.0f, "555 555 555", "15MM12345");
 		
-		resident.eats();	// inheriting from person class'
-		resident.setAddress("55"," Fifty-Fifth Street", "Toronto", "Ontario");
+		student.eats();	// inheriting from person class'
+		student.setAddress("55"," Fifty-Fifth Street", "Toronto", "Ontario");
 		System.out.println("\n");
-		System.out.println(resident.getAddress());
+		System.out.println(student.getAddress());
 		
-		resident.changeAddress("44"," Forty-Fourth Street", "Toronto", "Ontario");
+		student.changeAddress("44"," Forty-Fourth Street", "Toronto", "Ontario");
 		System.out.println("\n");
-		System.out.println(resident.getAddress());
+		System.out.println(student.getAddress());
+		
+		student.addCourse("COMP101", "Programming Fundamentals", "First Computer Course", 600.35);
+		student.addCourse("COMP102", "Programming Design", "First UML Course", 600.35);
+		student.dropCourse("Programming Design");
+		//student.dropCourse("Programming Fundamentals");
+		System.out.println(student.toString());
 
 	}
 
