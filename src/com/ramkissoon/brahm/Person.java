5f7c9677457850daf6677f516c9ddf6194744640
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-10-07 23:46:05 -0400

    added getters and setters

--- src/com/ramkissoon/brahm/Person.java
+++ src/com/ramkissoon/brahm/Person.java
@@ -6,6 +6,40 @@ public class Person {
 	private String _name;
 	private float _heightInInches;
 	private float _massInPounds;
+	
+	// 	GETTERS/SETTERS, ACCESSORS/MUTATORS
+	
+	public int getAge() {
+		return this._age;
+	}
+
+	public void setAge(int _age) {
+	this._age = _age;
+	}
+
+	public String getName() {
+		return this._name;
+	}
+	
+	public void setName(String _name) {
+		this._name = _name;
+	}
+	
+	public float getHeightInInches() {
+		return this._heightInInches;
+	}
+	
+	public void setHeightInInches(float _heightInInches) {
+		this._heightInInches = _heightInInches;
+	}
+
+	public float getMassInPounds() {
+		return this._massInPounds;
+	}
+	
+	public void setMassInPounds(float _massInPounds) {
+		this._massInPounds = _massInPounds;
+	}
 
 	// CONSTRUCTOR++++++++++++++++++++++++++++++
 	public Person(int _age, String _name, float _heightInInches, float _massInPounds) {
